1f4f7d7b4ec7888ee8a8d8725452dfd0a6835f8a
Agreem-stack <agrim100jain@gmail.com>
2025-01-08 07:02:14 +0000

    day 2

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,28 @@
-console.log("pop");
+let a;
+let b;
+let c;
+let d;
+let e;
+let f;
+let g;
+let h;
+let i;
+let j;
+let k;
+let l;
+let m;
+let n;
+let o;
+let p;
+let q;
+let r;
+let s;
+let t;
+let u;
+let v;
+let w;
+let x;
+let y;
+let z;
+
+console.table(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
