4604344d4d2fe7af01f94e4a44fccb1a0c69f0b3
Agreem-stack <agrim100jain@gmail.com>
2025-01-08 13:58:29 +0000

    day 2

---
+++ js1/datatypes.js
@@ -0,0 +1,44 @@
+// // let num2++ =100;
+// // let ++num =100;
+// // num++;
+// // console.log();
+
+
+
+// // increment 
+
+// let num = 10;
+
+// console.log(num++); // r 10  c 11  a 10
+// console.log(++num); // r 12  c 12  a 12
+// console.log(++num); // r
+// console.log(++num);
+// console.log(++num); // r 15  c 15  a 15
+
+// for (let index = 0; index < 2; index++) {
+//     let element = 12    ;
+    
+// }
+
+
+// // let cars = [maruti, suzuki, honda, hyundai,]
+// // let obj = {
+// //     key1 : "rat",
+// //     key2 : "gary",
+// // }
+// console.log(typeof obj);
+
+
+let num=67;
+console.log(num);
+
+let obj = {
+    key1 : "rat",
+    key2 : "gary",
+}
+
+num=obj;
+
+console.log(num);
+console.log(typeof num);
+console.log(typeof num);
