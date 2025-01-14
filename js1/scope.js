12a617fda8021e954e9cf73db931d85db4622fa9
Agreem-stack <agrim100jain@gmail.com>
2025-01-14 03:38:25 +0000

    day8

---
+++ js1/scope.js
@@ -0,0 +1,21 @@
+function one(username){
+    
+    if (true) {
+
+        console.log(`${username}`);
+        return username;
+        
+    }
+    
+}
+
+one("agreem@github.com");
+
+function addtwo(num) {
+
+    console.log(`after adding ${num} with 2 is   `);
+    
+    return num+2;
+    
+    
+}
